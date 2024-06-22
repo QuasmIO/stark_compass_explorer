e257d9fe432457f9b3de8265ba4fec802d8c6f1a
web3technologies <zach@web3technologies.io>
2024-06-22 11:48:10 -0500

    add back 4000

--- config/runtime.exs
+++ config/runtime.exs
@@ -110,8 +110,8 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST")
-  port = String.to_integer(System.get_env("PORT") || "80")
+  host = System.get_env("PHX_HOST") || "madarastark.com"
+  port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :starknet_explorer, StarknetExplorerWeb.Endpoint,
     server: true,
